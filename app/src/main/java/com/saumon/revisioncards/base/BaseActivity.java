c00b5521496c8bc273d1b03cc7ae0700031ab6b5
Saumon <mathieu.hay37@hotmail.fr>
2019-04-02 23:21:59 +0200

    Added ButterKnife

--- app/src/main/java/com/saumon/revisioncards/base/BaseActivity.java
+++ app/src/main/java/com/saumon/revisioncards/base/BaseActivity.java
@@ -2,13 +2,17 @@ package com.saumon.revisioncards.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(this.getLayoutContentViewID());
+        ButterKnife.bind(this);
     }
 
     public abstract int getLayoutContentViewID();
