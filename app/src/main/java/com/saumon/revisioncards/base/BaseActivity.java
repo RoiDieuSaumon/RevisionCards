56155c79332a26dff81ebd47e9735a8d9e8757f6
Saumon <mathieu.hay37@hotmail.fr>
2019-04-02 23:22:24 +0200

    Deleted useless import

--- app/src/main/java/com/saumon/revisioncards/base/BaseActivity.java
+++ app/src/main/java/com/saumon/revisioncards/base/BaseActivity.java
@@ -2,7 +2,6 @@ package com.saumon.revisioncards.base;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
