6a6a4825a7a1ddebbd056ebf66d4949f076a60f5
mhay <mhay@leni.fr>
2019-04-04 14:02:39 +0200

    Added buttons

--- app/src/main/java/com/saumon/revisioncards/cardsrevisionselector/CardsRevisionSelectorActivity.java
+++ app/src/main/java/com/saumon/revisioncards/cardsrevisionselector/CardsRevisionSelectorActivity.java
@@ -1,11 +1,22 @@
 package com.saumon.revisioncards.cardsrevisionselector;
 
+import android.content.Intent;
+
 import com.saumon.revisioncards.R;
 import com.saumon.revisioncards.base.BaseActivity;
+import com.saumon.revisioncards.cardsrevision.CardsRevisionActivity;
+
+import butterknife.OnClick;
 
 public class CardsRevisionSelectorActivity extends BaseActivity {
     @Override
     public int getLayoutContentViewID() {
         return R.layout.activity_cards_revision_selector;
+    }
+
+    @OnClick(R.id.activity_cards_revision_selector_review_btn)
+    public void onClickReviewButton() {
+        Intent intent = new Intent(this, CardsRevisionActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/saumon/revisioncards/home/HomeActivity.java
+++ app/src/main/java/com/saumon/revisioncards/home/HomeActivity.java
@@ -1,9 +1,14 @@
 package com.saumon.revisioncards.home;
 
+import android.content.Intent;
 import android.support.v7.widget.Toolbar;
 
 import com.saumon.revisioncards.R;
 import com.saumon.revisioncards.base.BaseActivity;
+import com.saumon.revisioncards.cardsmanager.CardsManagerActivity;
+import com.saumon.revisioncards.cardsrevisionselector.CardsRevisionSelectorActivity;
+
+import butterknife.OnClick;
 
 public class HomeActivity extends BaseActivity {
     @Override
@@ -15,5 +20,17 @@ public class HomeActivity extends BaseActivity {
     protected void configureToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @OnClick(R.id.activity_main_review_btn)
+    public void onClickReviewButton() {
+        Intent intent = new Intent(this, CardsRevisionSelectorActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.activity_main_cards_manage_btn)
+    public void onClickCardsManageButton() {
+        Intent intent = new Intent(this, CardsManagerActivity.class);
+        startActivity(intent);
     }
 }
