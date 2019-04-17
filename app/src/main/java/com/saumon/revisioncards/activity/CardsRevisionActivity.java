0bc398640dffd6a73979ad2d5db1788396b3ca40
mhay <mhay@leni.fr>
2019-04-17 14:06:44 +0200

    Creating borders and adding current card display on revision view

--- app/src/main/java/com/saumon/revisioncards/activity/CardsRevisionActivity.java
+++ app/src/main/java/com/saumon/revisioncards/activity/CardsRevisionActivity.java
@@ -1,10 +1,20 @@
 package com.saumon.revisioncards.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
 import com.saumon.revisioncards.R;
 
 public class CardsRevisionActivity extends BaseActivity {
     @Override
     public int getLayoutContentViewID() {
         return R.layout.activity_cards_revision;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((TextView) findViewById(R.id.activity_cards_revision_current_card_text)).setText("Fiche 1");
     }
 }
