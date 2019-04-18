cdd82fe217920032551fca1c73372efe7630044a
mhay <mhay@leni.fr>
2019-04-18 14:02:17 +0200

    Completing cards revision view

--- app/src/main/java/com/saumon/revisioncards/activity/CardsRevisionActivity.java
+++ app/src/main/java/com/saumon/revisioncards/activity/CardsRevisionActivity.java
@@ -2,6 +2,7 @@ package com.saumon.revisioncards.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.saumon.revisioncards.R;
@@ -16,5 +17,10 @@ public class CardsRevisionActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ((TextView) findViewById(R.id.activity_cards_revision_current_card_text)).setText("Fiche 1");
+    }
+
+    @Override
+    protected Toolbar getToolbar() {
+        return findViewById(R.id.activity_cards_revision_toolbar);
     }
 }
