66716e59e3d68f92bdf658046e1581cebabf4494
mhay <mhay@leni.fr>
2019-05-14 14:04:24 +0200

    Add file creation test on home activity create, request permissions missing

--- app/src/main/java/com/saumon/revisioncards/activity/HomeActivity.java
+++ app/src/main/java/com/saumon/revisioncards/activity/HomeActivity.java
@@ -1,13 +1,25 @@
 package com.saumon.revisioncards.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 
 import com.saumon.revisioncards.R;
+import com.saumon.revisioncards.utils.StorageUtils;
 
 import butterknife.OnClick;
 
 public class HomeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (StorageUtils.isExternalStorageWritable()) {
+            StorageUtils.setTextInStorage(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), this, "test.txt", "test", "test");
+        }
+    }
+
     @Override
     public int getLayoutContentViewID() {
         return R.layout.activity_home;
